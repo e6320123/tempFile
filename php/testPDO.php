266b4cd83aea133897f2e8a69d96434295aa53ff
e6320123 <ben780618@gmail.com>
2019-10-09 15:55:24 +0800

    s

--- php/testPDO.php
+++ php/testPDO.php
@@ -2,41 +2,94 @@
 $conn = new PDO('mysql:host=localhost;
                         dbname=cy_cinemas;
                         charset=utf8', 'root', ''); 
-// ----------------------save-----------------------
-// $frontData = isset($_POST['JSONData'])?$_POST['JSONData']:'no post';
-// $list = json_decode($frontData);    
-// $ticketsNum      = isset($_POST['foodData'])?$_POST['foodData']:'no post'; 
-// $foodDrinksNum   = isset($_POST['ticketData'])?$_POST['ticketData']:'no post';
-// $account         = isset($_POST['account'])?$_POST['account']: "Guest";    
-// $sql = 'INSERT INTO `order_details`(screenings_id,members_account,courts_id,seat,
-//     total_price,discounted_price,tickets_num,meals_num,name,phone,email) 
-//     VALUES (:a,:b,:c,:d,:e,:f,:g,:h,:i,:j,:k)'; 
-// $a=1;  
-// $c=1;
-// $stmt = $conn->prepare($sql); 
-// $stmt->execute(
-//     [
-//     ':a'=> $a,':b'=> $account,':c'=> $c,':d'=> $list->seat,':e'=> $list->total,
-//     ':f'=> $list->real,':g'=> $ticketsNum,':h'=> $foodDrinksNum,':i'=> $list->memberName,
-//     ':j'=> $list->phone,':k'=> $list->email
-//     ]
-// );
-// ------------------Test Fetch SQL-------------------
-// $ID = 12;
-$sql = 'SELECT * FROM `order_details`';  // WHERE `id`=:id' 
-$stmt=$conn->prepare($sql);
-// $stmt->bindParam(':id', $ID);
-$stmt->execute();
-$sqlData = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($sqlData); 
- // ------------------Check Field-------------------
-    // $ID = 12;
-    $sql = 'show tables';  // WHERE `id`=:id' 
-    $stmt=$conn->prepare($sql);
-    // $stmt->bindParam(':id', $ID);
-    $stmt->execute();
-    $sqlData = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($sqlData);
+                       
+$mysql = $_POST['SQL']; 
+switch ($mysql) {
+    case 'show':
+        // ------------------show tables------------------- 
+        $sql = 'show tables';   
+        $stmt=$conn->prepare($sql); 
+        $stmt->execute();
+        $sqlData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $showTable = [];
+        foreach ($sqlData as $key => $value) { 
+            array_push($showTable,$value["Tables_in_cy_cinemas"]); 
+        }  
+        echo "*******************showTables**********************";  
+        echo json_encode($showTable);
+        break;
+    case 'desc':
+        // ------------------Check Field------------------- 
+        $sql = 'DESC `order_details`';   
+        $stmt=$conn->prepare($sql); 
+        $stmt->execute();
+        $sqlData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $desc = [];
+        foreach ($sqlData as $key => $value) { 
+            array_push($desc,$value["Field"]); 
+        }  
+        echo "*********************fields************************"; 
+        echo json_encode($desc);   
+        break;
+    case 'select':
+        // ------------------SELECT-------------------
+        $ID = isset($_POST['ID'])?$_POST['ID']:0; 
+        $sql = 'SELECT * FROM `order_details`';   
+        if($ID)
+            $sql = 'SELECT * FROM `order_details` WHERE `id`=:id'; 
+        $stmt=$conn->prepare($sql);
+        if($ID)
+            $stmt->bindParam(':id', $ID);
+        $stmt->execute();
+        $sqlData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($sqlData); 
+        break;
+    case 'save':  
+    // ----------------------save-----------------------
+        $frontData = isset($_POST['JSONData'])?$_POST['JSONData']:'no post';
+        $list = json_decode($frontData);    
+        $ticketsNum      = isset($_POST['foodData'])?$_POST['foodData']:'no post'; 
+        $foodDrinksNum   = isset($_POST['ticketData'])?$_POST['ticketData']:'no post';
+        $account         = $list->accout == ""?"Guest":$list->accout;    
+        $sql = 'INSERT INTO `order_details` ( 
+         `screenings_id`,
+         `serial_number` ,
+         `members_account` ,
+         `courts_id`,
+         `seat` ,
+         `total_price` ,
+         `discounted_price` ,
+         `tickets_num` ,
+         `meals_num` ,
+         `name` ,
+         `phone` ,
+         `email` ) 
+        VALUES (:a,:b,:c,:d,:e,:f,:g,:h,:i,:j,:k,:l)'; 
+        $a=1;  //screenings_id
+        $c=1;  //courts_id 
+        $stmt = $conn->prepare($sql); 
+        $stmt->bindParam(':a',$a);
+        $stmt->bindParam(':b',$list->orderNumber); 
+        $stmt->bindParam(':c',$account); 
+        $stmt->bindParam(':d',$c); 
+        $stmt->bindParam(':e',$list->seat); 
+        $stmt->bindParam(':f',$list->total); 
+        $stmt->bindParam(':g',$list->real); 
+        $stmt->bindParam(':h',$ticketsNum); 
+        $stmt->bindParam(':i',$foodDrinksNum); 
+        $stmt->bindParam(':j',$list->memberName); 
+        $stmt->bindParam(':k',$list->phone); 
+        $stmt->bindParam(':l',$list->email);
+        $stmt->execute(); 
+        echo "Saved"; 
+        break; 
+    default: 
+        break;
+}
+
+
+ 
+    
 
 
 
